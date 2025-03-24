ad0c4f4f46553dbd0d1b989aa3a901b0ef056393
faizullahkhan1 <faiz.ullah.khan@outlook.com>
2025-03-24 11:10:57 +0500

    3rd

--- my-next-camera-app/next.config.ts
+++ my-next-camera-app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+const nextConfig = {
+  trailingSlash: false,
 };
+
+module.exports = nextConfig;
+
+
+
 
 export default nextConfig;
